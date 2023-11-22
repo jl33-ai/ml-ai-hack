d898e266c1868812571e16cc9c7b71eec402e1c6
HSL16F <Harrison.Langdon@outlook.com>
2023-11-22 16:01:25 +1100

    Added transport disuption option. Bit finicky

--- backend/webscraper.py
+++ backend/webscraper.py
@@ -5,7 +5,7 @@ Created by Harrison
 Note difference for yearly, daily and active calls
 Todo:
 Scrape ptv websites
-
+Add weather data
 """
 
 
@@ -14,12 +14,15 @@ import requests
 from bs4 import BeautifulSoup as Bs
 import re
 import pandas as pd
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 # PTV websites
@@ -41,6 +44,8 @@ headers = {
                        'Chrome/75.0.3770.142 Safari/537.36'
     }
 
+driver = webdriver.Chrome()
+
 """
 Function to make all generic request calls
 """
@@ -54,22 +59,56 @@ def soup_maker(url):
 function to call information for distruptions
 Call depending on live update or current daily updates
 """
-def ptv_disruptions(news=False, debug=False):
+def ptv_disruptions(news=False, transport_option="train"):
 
     disruption_url = "https://www.ptv.vic.gov.au/disruptions/disruptions-information/#"
-
-    # Set the path to the OperaDriver
-    opera_driver_path = 'C:/WebDrivers/operadriver.exe'
 
     # Set the location of the Opera Browser
     # options = webdriver.ChromeOptions()
     # options.binary_location = 'path/to/your/opera.exe'  # Example: 'C:/Program Files/Opera/launcher.exe'
 
-    # Initialize the WebDriver with the specified options
-    driver = webdriver.Chrome()
+    # Initialize the WebDriver with the specified option
+
+    # driver.get(disruption_url)
+
+    # Depending on the transport type affects distruption
+    select_transport_option(transport_option)
 
 
-    driver.get(disruption_url)
+    # raw_html = soup_maker(disruption_url)
+    # if raw_html:
+    #     print("Data scraped")
+    #     distruption_list = raw_html.find(id={"accordion-2"})
+    #     print(distruption_list)
+        # for disruption in distruption_list.find_all("li"):
+            
+        #     print(disruption.content)
+
+    return "Error occured in data extraction"
+
+
+def select_transport_option(option):
+    print(f"Getting distruption data for {option}")
+    driver.get('https://www.ptv.vic.gov.au/disruptions/disruptions-information/')
+    option_ids = {
+        "train": "tabtitle-0",
+        "tram": "tabtitle-1",
+        "bus": "tabtitle-2",
+        "vline": "tabtitle-3"
+    }
+    # Waiting for page to load
+    time.sleep(2)
+
+    button_id = option_ids.get(option.lower())
+    if not button_id:
+        print("Invalid option")
+        return
+
+    try:
+        button = driver.find_element(By.ID, button_id)
+        button.click()
+    except NoSuchElementException:
+        print("Button not found")
 
     try:
         accordion_button = WebDriverWait(driver, 10).until(
@@ -81,7 +120,6 @@ def ptv_disruptions(news=False, debug=False):
         raw_html = Bs(html_content, 'html.parser')
         
         # text processing
-        # print(raw_html)
         if raw_html:
             live_updates = raw_html.find("div", class_={"accordion LiveTravelUpdates__accordion"})
             # print(live_updates)
@@ -99,16 +137,5 @@ def ptv_disruptions(news=False, debug=False):
         driver.quit()
 
 
-    # raw_html = soup_maker(disruption_url)
-    # if raw_html:
-    #     print("Data scraped")
-    #     distruption_list = raw_html.find(id={"accordion-2"})
-    #     print(distruption_list)
-        # for disruption in distruption_list.find_all("li"):
-            
-        #     print(disruption.content)
-
-    return "Error occured in data extraction"
-
-
-ptv_disruptions()
+# ptv_disruptions()
+ptv_disruptions(transport_option="bus")
