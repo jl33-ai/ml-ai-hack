c3779cf4b8650cd7c36522ff4f8588c944656cbf
Justin <justinkhlee27@gmail.com>
2023-11-23 20:11:15 +1100

    new push

---
+++ TODO.md
@@ -0,0 +1,15 @@
+- [ ] Haystack
+
+- [ ] https://www.youtube.com/watch?v=OSt3sFT1i18
+https://github.com/Lewington-pitsos/ouchcover
+- [ ] run server on local machine
+- [ ] 
+
+- [ ] Streamlit Website
+    - [ ] functional chatbot 
+    - [ ] Fine tuned 
+
+    - [ ] Functions 
+
+
+- [ ] Video
--- full-pipeline/streamlit_app.py
+++ full-pipeline/streamlit_app.py
@@ -1,12 +1,20 @@
 import streamlit as st
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    openai_api_key = os.getenv('MY_VARIABLE')
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.title("🗺️ Dora Transport")
+
+st.caption("GNeghiurehgiusrfh")
+
+st.text('HGEh')
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -18,13 +26,12 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    #client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    #response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     
-    #msg = response.choices[0].message.content
-    msg = 'Yolanda Boxjump'
+    msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
 
