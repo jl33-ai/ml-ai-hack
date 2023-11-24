a3a5f2b3c5d4d1be631f198e870f582e67e65468
Will Holdsworth <qyp4l7a5@duck.com>
2023-11-24 21:05:28 +1100

    missed another spot

--- full-pipeline/features.py
+++ full-pipeline/features.py
@@ -42,7 +42,7 @@ DETAILS = [
 # return lat, long pair
 def getStartingLocation():
     response = requests.get("https://ipapi.co/json/").json()
-    return response['latitude'], response['longitude']
+    return response.latitude, response.longitude
 
 def getCurrentWeather(latitude, longitude):
     latitude, longitude = float(latitude), float(longitude)
