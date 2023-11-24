8624bd8218982ab59721b0b7ae2a07ce9b793739
Will Holdsworth <qyp4l7a5@duck.com>
2023-11-25 08:20:38 +1100

    add map

--- full-pipeline/streamlit_app.py
+++ full-pipeline/streamlit_app.py
@@ -39,7 +39,7 @@ with st.sidebar:
     #)
     
     # map
-    m = folium.Map([-37.8138889, 144.9630556])
+    m = folium.Map([-37.818240, 144.966396])
     st_folium(m, width=100)    
     "# Made by:"
     "🐧 Harrison"
