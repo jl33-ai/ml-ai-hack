a291849c774d133dabae0f69ae2a409c6840c954
Will Holdsworth <qyp4l7a5@duck.com>
2023-11-25 08:01:58 +1100

    add map

--- full-pipeline/features.py
+++ full-pipeline/features.py
@@ -96,7 +96,7 @@ DETAILS = [
 # return lat, long pair
 def getStartingLocation():
     response = requests.get("https://ipapi.co/json/").json()
-    return response['latitude'], response['longitude']
+    return response.latitude, response.longitude
 
 def getCurrentWeather():
     latitude, longitude = getStartingLocation()
